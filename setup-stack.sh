8906cf33c979a90ab0d4b65ded25339d338f73e8
katopz <katopz@gmail.com>
2017-02-27 21:46:17 +0700

    Try more delay.

--- setup-stack.sh
+++ setup-stack.sh
@@ -1,4 +1,7 @@
-# Creating network
+# Just for perf, you can skip this.
+START_SEC=$(date +'%s')
+
+# Creating network.
 docker-machine ssh node-1 "docker network create --driver overlay proxy"
 
 # Grab the source.
@@ -7,18 +10,27 @@ docker-machine ssh node-1 "curl -o docker-compose-stack.yml https://raw.githubus
 # Create docker-flow-proxy and docker-flow-swarm-listener services.
 docker-machine ssh node-1 "docker stack deploy -c docker-compose-stack.yml proxy"
 
-# Deploying More Stacks
+# Deploying More Stacks.
 docker-machine ssh node-1 "curl -o docker-compose-go-demo.yml https://raw.githubusercontent.com/vfarcic/go-demo/master/docker-compose-stack.yml"
 
 # Deploy.
 docker-machine ssh node-1 "docker stack deploy -c docker-compose-go-demo.yml go-demo"
 
+# See tasks of the stack.
+docker-machine ssh node-1 "docker stack ps proxy"
+
 # Verify from internal.
 docker-machine ssh node-1 "docker stack ps go-demo"
 
-# Wait for 3 sec to let server start properly.
-echo "Will wait for 3 seconds..."
-sleep 3
+# See how long does it take.
+echo "Done in $(($(date +'%s') - $START_SEC)) seconds"
+
+# Wait for 10 sec to let server start properly.
+echo "Will wait for 10 seconds..."
+sleep 10
 
 # Verify from external.
 docker-machine ssh node-1 'curl -i "127.0.0.1/demo/hello"'
+
+# Hm?
+echo "If curl don't work do wait a minite and try curl again."
